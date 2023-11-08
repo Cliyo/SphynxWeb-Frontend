5d489950827c4989e2985af19954d7a835691b93
Pedro <pedrohvidals@gmail.com>
2023-11-08 09:45:13 -0300

    [feat] - creating the menu and http request to delete a consumer

--- assets/js/consumerFunctions/consumerHttpRequests.js
+++ assets/js/consumerFunctions/consumerHttpRequests.js
@@ -1,6 +1,7 @@
 // CONSUMER FORMS //
 const formConsumerRegister = document.querySelector("#consumer-register-form");
 const formConsumerUpdate = document.querySelector("#consumer-update-form");
+const formConsumerDelete = document.querySelector("#consumer-delete-form");
 
 // HTTP REQUESTS TO THE API //
 formConsumerRegister.addEventListener("submit", (event) => {
@@ -83,5 +84,48 @@ formConsumerUpdate.addEventListener("submit", (event) => {
         let message = formConsumerUpdate.parentNode.querySelector("#alert");
         message.innerHTML = "Error...";
         message.style.color = "#FF0000";
+    })
+})
+
+formConsumerDelete.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    var formData =  new FormData(formConsumerDelete);
+    var data = Object.fromEntries(formData);
+
+    fetch(`http://localhost:8080/consumer/${data["ra"]}`, {
+        mode: "cors",
+        method: "DELETE",
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem("token")}`,
+            'Access-Control-Allow-Origin': 'http://localhost:8080',
+            'Access-Control-Allow-Credentials': 'true',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({})
+    })
+    .then(response => {
+        if(response.status != 204){
+            let message = formConsumerDelete.parentNode.querySelector("#alert");
+            message.innerHTML = "Error..."
+            message.style.color = "#FF0000";
+
+            throw new Error("HTTP Status " + response.status);
+        }
+        console.log(response);
+        console.log(response.status)
+        return response;
+    })
+    .then(data => {
+        let message = formConsumerDelete.parentNode.querySelector("#alert");
+        message.innerHTML = "Consumer deleted!";
+        message.style.color = "#00FF00";
+        window.location = "dashboardPage.html";
+    })
+    .catch(err => {
+        let message = formConsumerDelete.parentNode.querySelector("#alert");
+        message.innerHTML = "Error...";
+        message.style.color = "#FF0000";
+        console.log(err)
     })
 })
