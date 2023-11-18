8322e57263defd01dc7fbfd3c2b1e02244b35d53
Pedro <pedrohvidals@gmail.com>
2023-11-18 09:39:31 -0300

    [feat] - creating the http requests to get the access registers with filter

--- assets/js/accessFunctions/accessHttpRequests.js
+++ assets/js/accessFunctions/accessHttpRequests.js
@@ -1,8 +1,14 @@
 // TABLE //
-const accessGetAllTableData = accessGetAllDiv.querySelector(".content-table").querySelector("tbody");
+const accessGetTableData = accessGetDiv.querySelector(".content-table").querySelector("tbody");
+
+// BUTTONS //
+const accessAllButton = document.querySelector("#sub-item-access-all");
+const accessDailyButton = document.querySelector("#sub-item-access-daily");
+const accessLocalButton = document.querySelector("#sub-item-access-local");
+const accessConsumerButton = document.querySelector("#sub-item-access-consumer");
 
 // HTTP REQUESTS TO THE API //
-subItemAccessGetAll.addEventListener("click", (event) => {
+accessAllButton.addEventListener("click", (event) => {
     event.preventDefault()
     fetch(`http://localhost:8080/accessRegister`, {
         mode: "cors",
@@ -21,7 +27,7 @@ subItemAccessGetAll.addEventListener("click", (event) => {
         return response.json();
     })
     .then(data => {
-        accessGetAllTableData.innerHTML = "";
+        accessGetTableData.innerHTML = "";
         let array = Object.keys(data);
 
         array.forEach(index => {
@@ -52,7 +58,198 @@ subItemAccessGetAll.addEventListener("click", (event) => {
             tr.appendChild(tdLocal);
             tr.appendChild(tdDate);
             
-            accessGetAllTableData.appendChild(tr);
+            accessGetTableData.appendChild(tr);
+        })
+        
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+raInput.addEventListener("focusout", (event) => {
+    event.preventDefault()
+
+    let ra = raInput.value;
+
+    fetch(`http://localhost:8080/accessRegister/byRa/${ra}`, {
+        mode: "cors",
+        method: "GET",
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem("token")}`,
+            'Access-Control-Allow-Origin': 'http://localhost:8080',
+            'Access-Control-Allow-Credentials': 'true',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if(!response.ok){
+            throw new Error("HTTP Status " + response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        accessGetTableData.innerHTML = "";
+        let array = Object.keys(data);
+
+        array.forEach(index => {
+            let tr = document.createElement("tr");
+
+            let tdId = document.createElement("td");
+            tdId.innerHTML = data[index]["id"];
+
+            let tdRa = document.createElement("td");
+            tdRa.innerHTML = data[index]["ra"];
+
+            let tdNome = document.createElement("td");
+            tdNome.innerHTML = data[index]["name"];
+
+            let tdTag = document.createElement("td");
+            tdTag.innerHTML = data[index]["tag"];
+
+            let tdLocal = document.createElement("td");
+            tdLocal.innerHTML = data[index]["local"];
+
+            let tdDate = document.createElement("td");
+            tdDate.innerHTML = data[index]["date"];
+            
+            tr.appendChild(tdId);
+            tr.appendChild(tdRa);
+            tr.appendChild(tdNome);
+            tr.appendChild(tdTag);
+            tr.appendChild(tdLocal);
+            tr.appendChild(tdDate);
+            
+            accessGetTableData.appendChild(tr);
+        })
+        
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+localInput.addEventListener("focusout", (event) => {
+    event.preventDefault()
+
+    let local = localInput.value.replace(" ", "_");
+
+    fetch(`http://localhost:8080/accessRegister/byLocal/${local}`, {
+        mode: "cors",
+        method: "GET",
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem("token")}`,
+            'Access-Control-Allow-Origin': 'http://localhost:8080',
+            'Access-Control-Allow-Credentials': 'true',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if(!response.ok){
+            throw new Error("HTTP Status " + response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        accessGetTableData.innerHTML = "";
+        let array = Object.keys(data);
+
+        array.forEach(index => {
+            let tr = document.createElement("tr");
+
+            let tdId = document.createElement("td");
+            tdId.innerHTML = data[index]["id"];
+
+            let tdRa = document.createElement("td");
+            tdRa.innerHTML = data[index]["ra"];
+
+            let tdNome = document.createElement("td");
+            tdNome.innerHTML = data[index]["name"];
+
+            let tdTag = document.createElement("td");
+            tdTag.innerHTML = data[index]["tag"];
+
+            let tdLocal = document.createElement("td");
+            tdLocal.innerHTML = data[index]["local"];
+
+            let tdDate = document.createElement("td");
+            tdDate.innerHTML = data[index]["date"];
+            
+            tr.appendChild(tdId);
+            tr.appendChild(tdRa);
+            tr.appendChild(tdNome);
+            tr.appendChild(tdTag);
+            tr.appendChild(tdLocal);
+            tr.appendChild(tdDate);
+            
+            accessGetTableData.appendChild(tr);
+        })
+        
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+dateInput.addEventListener("focusout", (event) => {
+    event.preventDefault()
+
+    let date = new Date(dateInput.value);
+
+    let day = date.getDate() + 1;
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let dateComplete = year + "-" + month + "-" + day;
+
+    fetch(`http://localhost:8080/accessRegister/byDate/${dateComplete}`, {
+        mode: "cors",
+        method: "GET",
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem("token")}`,
+            'Access-Control-Allow-Origin': 'http://localhost:8080',
+            'Access-Control-Allow-Credentials': 'true',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if(!response.ok){
+            throw new Error("HTTP Status " + response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        accessGetTableData.innerHTML = "";
+        let array = Object.keys(data);
+
+        array.forEach(index => {
+            let tr = document.createElement("tr");
+
+            let tdId = document.createElement("td");
+            tdId.innerHTML = data[index]["id"];
+
+            let tdRa = document.createElement("td");
+            tdRa.innerHTML = data[index]["ra"];
+
+            let tdNome = document.createElement("td");
+            tdNome.innerHTML = data[index]["name"];
+
+            let tdTag = document.createElement("td");
+            tdTag.innerHTML = data[index]["tag"];
+
+            let tdLocal = document.createElement("td");
+            tdLocal.innerHTML = data[index]["local"];
+
+            let tdDate = document.createElement("td");
+            tdDate.innerHTML = data[index]["date"];
+            
+            tr.appendChild(tdId);
+            tr.appendChild(tdRa);
+            tr.appendChild(tdNome);
+            tr.appendChild(tdTag);
+            tr.appendChild(tdLocal);
+            tr.appendChild(tdDate);
+            
+            accessGetTableData.appendChild(tr);
         })
         
     })
