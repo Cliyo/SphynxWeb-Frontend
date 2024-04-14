4913b821b22de5391b568e881db1a90630fdcfc8
PedroVidalDev <pedrohvidals@gmail.com>
2024-04-14 19:56:35 -0300

    [refactor] - change the way to change the language

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -1,17 +1,4 @@
 import translateBr from "../json/translations/portuguese.json" assert { type: "json" };
-import translateEn from "../json/translations/english.json" assert {type: "json"};
-
-const check = document.querySelector(".checkbox");
-
-check.addEventListener("change", (event) => {
-    event.preventDefault();
-
-    if(check.checked == true){
-        changeToPortuguese();
-    } else {
-        changeToEnglish();
-    }
-})
 
 const titles = document.querySelectorAll("h1");
 const subitem = document.querySelectorAll(".sub-item");
@@ -19,7 +6,10 @@ const menus = document.querySelectorAll(".sub-btn");
 const columns = document.querySelectorAll("th");
 const buttons = document.querySelectorAll(".submit");
 
-function changeToPortuguese(){
+const urlParams = new URLSearchParams(window.location.search);
+const language = urlParams.get("language");
+
+if(language == "pt"){
     subitem.forEach(item => {
         let itemFormat = item.id.split("sub-item-")[1];
 
@@ -71,59 +61,3 @@ function changeToPortuguese(){
         button.value = "Finalizar";
     });
 }
-
-function changeToEnglish(){
-    subitem.forEach(item => {
-        let itemFormat = item.id.split("sub-item-")[1];
-
-        item.innerHTML = translateEn[itemFormat]; 
-
-    });
-
-    menus.forEach(menu => {
-        let i = document.createElement("i");
-        i.className = "fa-solid fa-chevron-right dropdown";
-         
-        if("access" in menu.dataset){
-            menu.innerHTML = '<i class="fa-solid fa-chevron-right dropdown"></i> ' + translateEn.access;
-        }
-
-        if("consumer" in menu.dataset){
-            menu.innerHTML = '<i class="fa-solid fa-chevron-right dropdown"></i> ' + translateEn.consumer;
-        }
-
-        if("local" in menu.dataset){
-            menu.innerHTML = '<i class="fa-solid fa-chevron-right dropdown"></i> ' + translateEn.local;
-        }
-    });
-    
-    columns.forEach(column => {
-        if("name" in column.dataset){
-            column.innerHTML = translateEn.name;
-        }
-
-        if("date" in column.dataset){
-            column.innerHTML = translateEn.date;
-        }
-    });
-
-    titles.forEach(title => {
-        if("register" in title.dataset){
-            title.innerHTML = translateEn["consumer-register"]
-        }
-
-        if("update" in title.dataset){
-            title.innerHTML = translateEn["consumer-update"];
-        }
-
-        if("delete" in title.dataset){
-            title.innerHTML = translateEn["consumer-delete"];
-        }
-    });
-
-    buttons.forEach(button => {
-        button.value = "Finalize";
-    });
-}
-
-
