1521a9a700a357b34b084e66bfcffb250e9e1c33
Snootic <snooticonoob@hotmail.com>
2024-04-14 23:00:25 -0300

    feat: add english translations and make the code more readable

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -2,29 +2,44 @@ import { language } from "./dashboardScript.js";
 
 var userLanguage = language || navigator.language || navigator.userLanguage;
 console.log(userLanguage);
-var elements = document.querySelectorAll('*')
-console.log(elements)
+
+var elements = document.querySelectorAll('*');
+
+function updateInputElement(element, key, value) {
+    if (element.tagName === 'INPUT') {
+        if (element.type === 'text' || element.type === 'password') {
+            element.placeholder = value;
+        } else if (element.type === 'submit') {
+            element.value = value;
+        }
+    }
+}
+
+function updateNonInputElement(element, key, value) {
+    element.childNodes.forEach(child => {
+        if (child.nodeType === Node.TEXT_NODE && child.textContent.trim() !== "") {
+            child.textContent = value;
+        }
+    });
+}
 
 fetch('../assets/i18n/' + userLanguage + '.json')
     .then(response => response.json())
     .then(data => {
         Object.keys(data).forEach(key => {
-            if(elements.length > 1){
-                elements.forEach(element => {
-                    if (element.id === key){
-                        if (element.tagName === 'INPUT' && (element.type === 'text' || element.type === 'password')) {
-                        element.placeholder = data[key];
-                        } else if (element.tagName === 'INPUT' && element.type === 'submit') {
-                            element.value = data[key];
-                        } else {
-                            element.textContent = data[key];
-                        }
+            elements.forEach(element => {
+                if (element.id === key) {
+                    switch (element.tagName) {
+                        case 'INPUT':
+                            updateInputElement(element, key, data[key]);
+                            break;
+                        default:
+                            updateNonInputElement(element, key, data[key]);
                     }
-                })
-            } 
+                }
+            });
         });
     })
     .catch(error => {
         console.error('Error loading language file:', error);
     });
-
