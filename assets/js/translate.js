f38073c6b7f1c9f74b8c52599aab97bfcd54399c
PedroVidalDev <pedrohvidals@gmail.com>
2024-04-14 21:37:58 -0300

    [fix] - trying to fix the translate error

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -8,6 +8,20 @@ fetch('../assets/i18n/' + userLanguage + '.json')
     .then(response => response.json())
     .then(data => {
         Object.keys(data).forEach(key => {
+            
+            var elements = document.querySelectorAll(key);
+            if(element.length > 1){
+                elements.forEach(element => {
+                    if (element.tagName === 'INPUT' && (element.type === 'text' || element.type === 'password')) {
+                        element.placeholder = data[key];
+                    } else if (element.tagName === 'INPUT' && element.type === 'submit') {
+                        element.value = data[key];
+                    } else {
+                        element.textContent = data[key];
+                    }
+                })
+            }
+
             var element = document.getElementById(key);
             if (element) {
                 if (element.tagName === 'INPUT' && (element.type === 'text' || element.type === 'password')) {
