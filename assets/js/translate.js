6631f5042eb8e6401bc4cad46c5bf26f303bb451
Snootic <snooticonoob@hotmail.com>
2024-04-14 21:56:57 -0300

    fix[translations]: fix all translations in the website

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -2,38 +2,26 @@ import { language } from "./dashboardScript.js";
 
 var userLanguage = language || navigator.language || navigator.userLanguage;
 console.log(userLanguage);
-var elements = document.getElementsByTagName('*');
+var elements = document.querySelectorAll('*')
+console.log(elements)
 
 fetch('../assets/i18n/' + userLanguage + '.json')
     .then(response => response.json())
     .then(data => {
         Object.keys(data).forEach(key => {
-
-            var elements = document.querySelectorAll(key);
             if(elements.length > 1){
                 elements.forEach(element => {
-                    if (element.tagName === 'INPUT' && (element.type === 'text' || element.type === 'password')) {
+                    if (element.id === key){
+                        if (element.tagName === 'INPUT' && (element.type === 'text' || element.type === 'password')) {
                         element.placeholder = data[key];
-                    } else if (element.tagName === 'INPUT' && element.type === 'submit') {
-                        element.value = data[key];
-                    } else {
-                        element.textContent = data[key];
+                        } else if (element.tagName === 'INPUT' && element.type === 'submit') {
+                            element.value = data[key];
+                        } else {
+                            element.textContent = data[key];
+                        }
                     }
                 })
-            } else{
-                var element = document.getElementById(key);
-                if (element) {
-                    if (element.tagName === 'INPUT' && (element.type === 'text' || element.type === 'password')) {
-                        element.placeholder = data[key];
-                    } else if (element.tagName === 'INPUT' && element.type === 'submit') {
-                        element.value = data[key];
-                    } else {
-                        element.textContent = data[key];
-                    }
-                }
-            }
-
-            
+            } 
         });
     })
     .catch(error => {
