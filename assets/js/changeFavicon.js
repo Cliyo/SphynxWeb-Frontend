a2fee49008ea670b9fbc8c7149168a359318300c
Snootic <snooticonoob@hotmail.com>
2024-04-15 08:57:59 -0300

    feat: add favicon

---
+++ assets/js/changeFavicon.js
@@ -0,0 +1,15 @@
+var link = document.querySelector("link[rel~='icon']");
+
+function changeFavicon(){
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        link.href = '../assets/img/favicon-light.ico';
+    }
+    else if (window.matchMedia('(prefers-color-scheme: light)').matches){
+        link.href = '../assets/img/favicon-dark.ico';
+    }
+}
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    changeFavicon();
+});
+
+changeFavicon();
