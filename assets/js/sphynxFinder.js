f305d35a347e676d2e8c78282dccb4e05652b918
PedroVidalDev <pedrohvidals@gmail.com>
2024-04-16 14:09:06 -0300

    [feat] - adding a try catch in sphynx finder req

--- assets/js/sphynxFinder.js
+++ assets/js/sphynxFinder.js
@@ -15,12 +15,17 @@ async function finder(){
     })
     .then(data => {
         data.forEach(esp => {
-            let ws = new WebSocket(`ws://${esp.ip}/ws`);
-            ws.onmessage = (data) => {
-                if(data.data == "$2a$12$X9my8HHbMJYk6y04FnR6ie1B/WnLOlBAeEMRhEOvt.8z/OmOR6kLS"){
-                    arrayEsp.push(ws);
+            try{
+                let ws = new WebSocket(`ws://${esp.ip}/ws`);
+                ws.onmessage = (data) => {
+                    if(data.data == "$2a$12$X9my8HHbMJYk6y04FnR6ie1B/WnLOlBAeEMRhEOvt.8z/OmOR6kLS"){
+                        arrayEsp.push(ws);
+                    }
                 }
+            } catch(error){
+                console.error("Error: ", error);
             }
+            
         });
 
         return arrayEsp;
