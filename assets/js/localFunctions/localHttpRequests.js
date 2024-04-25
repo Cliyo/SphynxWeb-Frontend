790a4dd5c56bc85d1d598ffa2bac74fadb0cc5fa
PedroVidalDev <pedrohvidals@gmail.com>
2024-04-25 20:36:53 -0300

    refactor: using requestHttp function in localhttprequests

--- assets/js/localFunctions/localHttpRequests.js
+++ assets/js/localFunctions/localHttpRequests.js
@@ -1,4 +1,5 @@
 import { header, IP } from "../dashboardScript.js";
+import request from "../utils/requestHttp.js";
 import { localGetDiv, subItemLocalGet } from "./localMenus.js";
 
 // LOCAL FORMS //
@@ -10,117 +11,72 @@ const localsTableData = localGetDiv.querySelector(".content-table").querySelecto
 
 // HTTP REQUESTS TO THE API //
 
-formLocalUpdate.addEventListener("submit", (event) => {
+formLocalUpdate.addEventListener("submit", async (event) => {
     event.preventDefault()
 
     var formData =  new FormData(formLocalUpdate);
     var data = Object.fromEntries(formData);
+    var jsonData = JSON.stringify({"mac": data["mac"]});
 
-    fetch(`http://${IP}:8080/local/${data["name"]}`, {
-        mode: "cors",
-        method: "PUT",
-        headers: header,
-        body: JSON.stringify({"mac": data["mac"]})
-    })
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        if(data.message == 400){
-            let message = formLocalUpdate.parentNode.querySelector("#alert");
-            message.innerHTML = data.message;
-            message.style.color = "#FF0000";
-        } else{
-            let message = formLocalUpdate.parentNode.querySelector("#alert");
-            message.innerHTML = data.message;
-            message.style.color = "#00FF00";
-            window.location = "dashboardPage.html";
-        }
+    const reqData = await request(IP, `local/${data["name"]}`, "PUT", header, jsonData);
 
-    })
-    .catch(err => {
+    if(reqData.message == 400){
         let message = formLocalUpdate.parentNode.querySelector("#alert");
-        message.innerHTML = "Error...";
+        message.innerHTML = reqData.message;
         message.style.color = "#FF0000";
-    })
+    } else{
+        let message = formLocalUpdate.parentNode.querySelector("#alert");
+        message.innerHTML = reqData.message;
+        message.style.color = "#00FF00";
+        window.location = "dashboardPage.html";
+    }
 })
 
-formLocalDelete.addEventListener("submit", (event) => {
+formLocalDelete.addEventListener("submit", async (event) => {
     event.preventDefault()
 
     var formData =  new FormData(formLocalDelete);
     var data = Object.fromEntries(formData);
+    var jsonData = JSON.stringify({});
 
-    fetch(`http://${IP}:8080/local/${data["name"]}`, {
-        mode: "cors",
-        method: "DELETE",
-        headers: header,
-        body: JSON.stringify({})
-    })
-    .then(response => {
-        if(response.status != 204){
-            let message = formLocalDelete.parentNode.querySelector("#alert");
-            message.innerHTML = "Error..."
-            message.style.color = "#FF0000";
-
-            throw new Error("HTTP Status " + response.status);
-        }
-        console.log(response);
-        console.log(response.status)
-        return response;
-    })
-    .then(data => {
+    try{
+        const reqData = await request(IP, `local/${data["name"]}`, "DELETE", header, jsonData);
         let message = formLocalDelete.parentNode.querySelector("#alert");
         message.innerHTML = "Local deleted!";
         message.style.color = "#00FF00";
         window.location = "dashboardPage.html";
-    })
-    .catch(err => {
+    } catch(e){
         let message = formLocalDelete.parentNode.querySelector("#alert");
         message.innerHTML = "Error...";
         message.style.color = "#FF0000";
         console.log(err)
-    })
+    }
 })
 
-subItemLocalGet.addEventListener("click", (event) => {
+subItemLocalGet.addEventListener("click", async (event) => {
     event.preventDefault()
-    fetch(`http://${IP}:8080/local`, {
-        mode: "cors",
-        method: "GET",
-        headers: header
-    })
-    .then(response => {
-        if(!response.ok){
-            throw new Error("HTTP Status " + response.status);
-        }
-        return response.json();
-    })
-    .then(data => {
-        localsTableData.innerHTML = "";
-        let array = Object.keys(data);
 
-        array.forEach(index => {
-            let tr = document.createElement("tr");
+    const reqData = await request(IP, `local`, "GET", header, null);
 
-            let tdId = document.createElement("td");
-            tdId.innerHTML = data[index]["id"];
+    localsTableData.innerHTML = "";
+    let array = Object.keys(reqData);
 
-            let tdName = document.createElement("td");
-            tdName.innerHTML = data[index]["name"];
+    array.forEach(index => {
+        let tr = document.createElement("tr");
 
-            let tdMac = document.createElement("td");
-            tdMac.innerHTML = data[index]["mac"];
-            
-            tr.appendChild(tdId);
-            tr.appendChild(tdName);
-            tr.appendChild(tdMac);
-            
-            localsTableData.appendChild(tr);
-        })
+        let tdId = document.createElement("td");
+        tdId.innerHTML = reqData[index]["id"];
+
+        let tdName = document.createElement("td");
+        tdName.innerHTML = reqData[index]["name"];
+
+        let tdMac = document.createElement("td");
+        tdMac.innerHTML = reqData[index]["mac"];
         
-    })
-    .catch(err => {
-        console.log(err)
+        tr.appendChild(tdId);
+        tr.appendChild(tdName);
+        tr.appendChild(tdMac);
+        
+        localsTableData.appendChild(tr);
     })
 })
