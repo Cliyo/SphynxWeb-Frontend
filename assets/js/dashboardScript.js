ad5497cd2d3db4005aa84c7c46a83b29505523ab
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-05 17:33:54 -0300

    feat: criando anmacao para mostrar menu ao clickar no botao

--- assets/js/dashboardScript.js
+++ assets/js/dashboardScript.js
@@ -1,31 +1,15 @@
-import { fetchEspData } from "./localFunctions/localMenus.js";
+/*import { fetchEspData } from "./localFunctions/localMenus.js";
 
 const api = `${window.location.hostname}:57128`;
 const apiUrls = ['sphynx-api.local:57128','localhost:57128', `${window.location.hostname}:57128`]
-// api = await testConnection(apiUrls)
 
-// MENU ANIMATION //
-$('.sub-btn').next('.sub-menu').slideToggle();
-$(document).ready(function() {
-    if (window.innerWidth <= 650) { 
-        $('.menu-button').next('.menu').slideToggle();
-        $('.menu-button').click(function() {
-            $('.menu').slideToggle();
-            $(this).find('.dropdown').toggleClass('rotate');
-        });
-    }
-    
-    $('.sub-btn').click(function() {
-        $('.sub-menu').not($(this).next('.sub-menu')).slideUp();
-        $('.dropdown').not($(this).find('.dropdown')).removeClass('rotate');
-        $(this).next('.sub-menu').slideToggle();
-        $(this).find('.dropdown').toggleClass('rotate');
-    });
-});
-
-// GET THE WEBSITE LANGUAGE //
 const urlParams = new URLSearchParams(window.location.search);
 const language = urlParams.get("language");
+
+// api = await testConnection(apiUrls)
+
+// GET THE WEBSITE LANGUAGE //
+
 
 // SWITCH THE WEBSITE FONT STYLE //
 const htmlDocument = document.querySelector("html");
@@ -39,7 +23,6 @@ switchButton.addEventListener("click", () => {
     }
 })
 
-
 // DEFINE THE HTTP HEADER //
 const header = {
     'Authorization': `Bearer ${localStorage.getItem("token")}`,
@@ -48,6 +31,7 @@ const header = {
     'Content-Type': 'application/json',
     'language': language
 }
+
 
 // VERIFY THE USER LOGIN //
 if(localStorage.getItem("token")){
@@ -77,7 +61,21 @@ if(localStorage.getItem("token")){
 } else{
     window.location = "/";
 }
+*/
+const botaoMenuMobile = document.querySelector("#menu-mobile-botao");
+botaoMenuMobile.addEventListener("click", () => {
+    let nav = document.querySelector("nav");
 
-fetchEspData(true);
+    if(nav.classList.contains("mostrarMenu")){
+        nav.classList.remove("mostrarMenu")
+        nav.classList.add("esconderMenu")
+    }
+    else{
+        nav.classList.remove("esconderMenu")
+        nav.classList.add("mostrarMenu")
+    }
+})
 
-export {header, language, api};
+//fetchEspData(true);
+
+//export {header, language, api};
