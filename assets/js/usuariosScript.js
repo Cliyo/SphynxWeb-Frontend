1c823b0100aa4de1bce74639253f50356a1d88ec
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-07 18:44:59 -0300

    feat: criando funcao para adicionar e remover usuario da tela

--- assets/js/usuariosScript.js
+++ assets/js/usuariosScript.js
@@ -1,8 +1,14 @@
+const usuariosContainer = document.querySelector("#usuarios-container");
+
 const opcaoUsuarioVer = document.querySelector("#usuarios-menu-ver");
 const opcaoUsuarioGrupo = document.querySelector("#usuarios-menu-grupo");
 
 const menuGeral = document.querySelector("#usuarios-ver");
 const inputGrupo = document.querySelector("#input-grupo");
+
+const botaoCadastrarUsuario = document.querySelector("#botao-adicionar-novo");
+const divCadastrarUsuario = document.querySelector("#usuarios-cadastrar");
+const botaoCancelarCadastro = document.querySelector("#cancelar-cadastrar");
 
 opcaoUsuarioVer.addEventListener("click", () => {
     if(!opcaoUsuarioVer.classList.contains("selecionado")){
@@ -21,4 +27,14 @@ opcaoUsuarioGrupo.addEventListener("click", () => {
 
         inputGrupo.style.display = "flex";
     }
+})
+
+botaoCadastrarUsuario.addEventListener("click", () => {
+    usuariosContainer.classList.add("escurecer");
+    divCadastrarUsuario.classList.add("mostrar");
+})
+
+botaoCancelarCadastro.addEventListener("click", () => {
+    usuariosContainer.classList.remove("escurecer");
+    divCadastrarUsuario.classList.remove("mostrar");
 })
