ba8f6ec0a3dd42d7108c720530b32b881bd5699f
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-08 10:26:34 -0300

    feat: criando script em locais para adicionar linhas a tabela de "ver"

--- assets/js/locaisScript.js
+++ assets/js/locaisScript.js
@@ -10,6 +10,34 @@ opcaoLocalVer.addEventListener("click", () => {
         opcaoLocalVer.classList.toggle("selecionado");
         opcaoLocalCadastrar.classList.toggle("selecionado");
 
+        let tr = document.createElement("tr");
+
+        let tdNome = document.createElement("td");
+        tdNome.innerHTML = "LAB10";
+
+        let tdPermissao = document.createElement("td");
+        tdPermissao.innerHTML = "1 - Aluno";
+
+        let tdMac = document.createElement("td");
+        tdMac.innerHTML = "11:11:11:11:11:11";
+
+        let tdAcao = document.createElement("td");
+
+        let botaoEditar = document.createElement("button");
+        botaoEditar.innerHTML = "Editar";
+
+        let botaoExcluir = document.createElement("button");
+        botaoExcluir.innerHTML = "Excluir";
+
+        tdAcao.appendChild(botaoEditar);
+        tdAcao.appendChild(botaoExcluir);
+
+        tr.appendChild(tdNome);
+        tr.appendChild(tdPermissao);
+        tr.appendChild(tdMac);
+        tr.appendChild(tdAcao);
+
+        tabela.append(tr);
     }
     
 })
