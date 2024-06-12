9941dbf6c15fbc29041d376c4cf58848e4d5d8dc
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-11 23:15:01 -0300

    refactor: criando funcao para criar linha para preencher tabela

--- assets/js/usuariosScript.js
+++ assets/js/usuariosScript.js
@@ -16,6 +16,7 @@ const botaoCancelarCadastro = document.querySelector("#cancelar-cadastrar");
 
 const tabela = document.querySelector("tbody");
 
+
 opcaoUsuarioVer.addEventListener("click", async () => {
     if(!opcaoUsuarioVer.classList.contains("selecionado")){
         opcaoUsuarioVer.classList.toggle("selecionado");
@@ -27,37 +28,9 @@ opcaoUsuarioVer.addEventListener("click", async () => {
         const response = await request(api, "consumers", "GET", headerAuth, null);
 
         response.forEach(usuario => {
-            let tr = document.createElement("tr");
+            let linha = criarLinhaTabela(usuario);
 
-            let tdNome = document.createElement("td");
-            tdNome.innerHTML = usuario.name;
-            
-            let tdGrupo = document.createElement("td");
-            tdGrupo.innerHTML = usuario.permission.name;
-
-            let tdRa = document.createElement("td");
-            tdRa.innerHTML = usuario.ra;
-
-            let tdTag = document.createElement("td");
-            tdTag.innerHTML = usuario.tag;
-
-            let tdAcao = document.createElement("td");
-
-            let editarBotao = document.createElement("button");
-            editarBotao.innerHTML = "Editar";
-            let excluirBotao = document.createElement("button");
-            excluirBotao.innerHTML = "Excluir";
-
-            tdAcao.appendChild(editarBotao);
-            tdAcao.appendChild(excluirBotao);
-
-            tr.appendChild(tdNome);
-            tr.appendChild(tdGrupo);
-            tr.appendChild(tdRa);
-            tr.appendChild(tdTag);
-            tr.appendChild(tdAcao);
-
-            tabela.appendChild(tr);
+            tabela.appendChild(linha);
         });
     }
     
@@ -81,3 +54,37 @@ botaoCancelarCadastro.addEventListener("click", () => {
     usuariosContainer.classList.remove("escurecer");
     divCadastrarUsuario.classList.remove("mostrar");
 })
+
+function criarLinhaTabela(usuario){
+    let tr = document.createElement("tr");
+
+    let tdNome = document.createElement("td");
+    tdNome.innerHTML = usuario.name;
+    
+    let tdGrupo = document.createElement("td");
+    tdGrupo.innerHTML = usuario.permission.name;
+
+    let tdRa = document.createElement("td");
+    tdRa.innerHTML = usuario.ra;
+
+    let tdTag = document.createElement("td");
+    tdTag.innerHTML = usuario.tag;
+
+    let tdAcao = document.createElement("td");
+
+    let editarBotao = document.createElement("button");
+    editarBotao.innerHTML = "Editar";
+    let excluirBotao = document.createElement("button");
+    excluirBotao.innerHTML = "Excluir";
+
+    tdAcao.appendChild(editarBotao);
+    tdAcao.appendChild(excluirBotao);
+
+    tr.appendChild(tdNome);
+    tr.appendChild(tdGrupo);
+    tr.appendChild(tdRa);
+    tr.appendChild(tdTag);
+    tr.appendChild(tdAcao);
+
+    return tr;
+}
