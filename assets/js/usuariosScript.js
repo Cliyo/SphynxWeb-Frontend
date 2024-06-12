752622053aa2d56a81b4b3c3cb8816c8a07fe280
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-11 23:10:25 -0300

    feat: fazendo requisicao para API quando botao eh clickado, resgatando os usuarios

--- assets/js/usuariosScript.js
+++ assets/js/usuariosScript.js
@@ -1,3 +1,7 @@
+import { headerAuth } from "./utils/headers.js";
+import { request } from "./utils/requestHttp.js";
+import { api } from "./utils/testeConexao.js";
+
 const usuariosContainer = document.querySelector("#usuarios-container");
 
 const opcaoUsuarioVer = document.querySelector("#usuarios-menu-ver");
@@ -10,12 +14,51 @@ const botaoCadastrarUsuario = document.querySelector("#botao-adicionar-novo");
 const divCadastrarUsuario = document.querySelector("#usuarios-cadastrar");
 const botaoCancelarCadastro = document.querySelector("#cancelar-cadastrar");
 
-opcaoUsuarioVer.addEventListener("click", () => {
+const tabela = document.querySelector("tbody");
+
+opcaoUsuarioVer.addEventListener("click", async () => {
     if(!opcaoUsuarioVer.classList.contains("selecionado")){
         opcaoUsuarioVer.classList.toggle("selecionado");
         opcaoUsuarioGrupo.classList.toggle("selecionado");
 
         inputGrupo.style.display = "none";
+
+        tabela.innerHTML = "";
+        const response = await request(api, "consumers", "GET", headerAuth, null);
+
+        response.forEach(usuario => {
+            let tr = document.createElement("tr");
+
+            let tdNome = document.createElement("td");
+            tdNome.innerHTML = usuario.name;
+            
+            let tdGrupo = document.createElement("td");
+            tdGrupo.innerHTML = usuario.permission.name;
+
+            let tdRa = document.createElement("td");
+            tdRa.innerHTML = usuario.ra;
+
+            let tdTag = document.createElement("td");
+            tdTag.innerHTML = usuario.tag;
+
+            let tdAcao = document.createElement("td");
+
+            let editarBotao = document.createElement("button");
+            editarBotao.innerHTML = "Editar";
+            let excluirBotao = document.createElement("button");
+            excluirBotao.innerHTML = "Excluir";
+
+            tdAcao.appendChild(editarBotao);
+            tdAcao.appendChild(excluirBotao);
+
+            tr.appendChild(tdNome);
+            tr.appendChild(tdGrupo);
+            tr.appendChild(tdRa);
+            tr.appendChild(tdTag);
+            tr.appendChild(tdAcao);
+
+            tabela.appendChild(tr);
+        });
     }
     
 })
