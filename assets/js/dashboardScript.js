402256f1a8a850dd565148fbfd2607a4d67cf896
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-17 22:37:15 -0300

    feat: criando script para pegar quantidade de aprovados  e alterar grafico

--- assets/js/dashboardScript.js
+++ assets/js/dashboardScript.js
@@ -2,11 +2,13 @@ import { headerAuth } from "./utils/headers.js";
 import { request } from "./utils/requestHttp.js";
 import { api } from "./utils/testeConexao.js";
 
+const graficoAcessos = document.getElementById("grafico-acessos");
 const quantidadeAcessos = document.querySelector("#quantidade-acessos");
 
 const response = await request(api, `accessRegisters?date=${montarData()}`, "GET", headerAuth, null);
 
 quantidadeAcessos.innerHTML = `Acessos (Hoje): ${response.length}`;
+graficoAcessos.style.background = `conic-gradient(#ff0000 0deg ${(360 * (pegarPorcentagemAprovados())) / 100}, #1eff00 ${(360 * (pegarPorcentagemAprovados())) / 100} 360deg)`
 
 function montarData(){
     let data = new Date();
@@ -15,4 +17,10 @@ function montarData(){
     let mes = (data.getMonth() + 1) < 10 ? `0${data.getMonth() + 1}` : data.getMonth() + 1;
 
     return data.getFullYear() + "-" + mes + "-" + dia;
+}
+
+function pegarPorcentagemAprovados(){
+    let aprovados = response.filter(acesso => acesso.status == true);
+
+    return (aprovados / response.length)  * 100;
 }
